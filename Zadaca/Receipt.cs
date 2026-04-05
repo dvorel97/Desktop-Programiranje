4b2c22dff16fd775cf8235aed7d5759d377f8099
David Vorel <dvorel@vub.hr>
2026-04-05 13:55:05 +0200

    inicijalna Receipt klasa napravljena

--- Zadaca/Receipt.cs
+++ Zadaca/Receipt.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace Zadaca
 {
-    internal class Receipt
+    public class Receipt
     {
+        private List<IProduct> items = new();
+        private decimal sum;
+        
+        public Receipt()
+        {
+            this.sum = 0;
+        }
+
+        public void AddItem(IProduct product)
+        {
+            items.Add(product);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach(var item in items)
+            {
+                sb.AppendLine($"{item.Name} ({item.Quantity}{item.MeasuringUnit}) - {item.Price}");
+            }
+            return sb.ToString();
+        }
+         
+    
     }
 }
