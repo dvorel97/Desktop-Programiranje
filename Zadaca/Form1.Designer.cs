be713c5165165aa0dbd3bd2035aa63a76ccd0f7f
David Vorel <dvorel@vub.hr>
2026-04-05 13:30:53 +0200

    form mockup done

--- Zadaca/Form1.Designer.cs
+++ Zadaca/Form1.Designer.cs
@@ -28,19 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblHeader = new Label();
+            groupBox1 = new GroupBox();
+            btnLatte = new Button();
+            btnCappucino = new Button();
+            btnEspresso = new Button();
+            groupBox2 = new GroupBox();
+            btnFanta = new Button();
+            btnSprite = new Button();
+            btnCola = new Button();
+            groupBox3 = new GroupBox();
+            btnSweets = new Button();
+            btnIceCream = new Button();
+            groupBox4 = new GroupBox();
+            btnCupon100 = new Button();
+            btnClose = new Button();
+            richTextBox1 = new RichTextBox();
+            lblSumTxt = new Label();
+            lblSum = new Label();
+            btnNew = new Button();
+            btnCheckout = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
             SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            lblHeader.AutoSize = true;
+            lblHeader.Font = new Font("MV Boli", 15.9000006F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblHeader.Location = new Point(23, 32);
+            lblHeader.Name = "lblHeader";
+            lblHeader.Size = new Size(295, 70);
+            lblHeader.TabIndex = 0;
+            lblHeader.Text = "VUB Caffe";
+            lblHeader.Click += label1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnLatte);
+            groupBox1.Controls.Add(btnCappucino);
+            groupBox1.Controls.Add(btnEspresso);
+            groupBox1.Location = new Point(78, 145);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(871, 182);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Kave";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // btnLatte
+            // 
+            btnLatte.Location = new Point(554, 46);
+            btnLatte.Name = "btnLatte";
+            btnLatte.Size = new Size(204, 105);
+            btnLatte.TabIndex = 2;
+            btnLatte.Text = "Latte";
+            btnLatte.UseVisualStyleBackColor = true;
+            // 
+            // btnCappucino
+            // 
+            btnCappucino.Location = new Point(304, 46);
+            btnCappucino.Name = "btnCappucino";
+            btnCappucino.Size = new Size(204, 105);
+            btnCappucino.TabIndex = 1;
+            btnCappucino.Text = "Cappucino";
+            btnCappucino.UseVisualStyleBackColor = true;
+            // 
+            // btnEspresso
+            // 
+            btnEspresso.Location = new Point(52, 46);
+            btnEspresso.Name = "btnEspresso";
+            btnEspresso.Size = new Size(204, 105);
+            btnEspresso.TabIndex = 0;
+            btnEspresso.Text = "Espresso";
+            btnEspresso.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnFanta);
+            groupBox2.Controls.Add(btnSprite);
+            groupBox2.Controls.Add(btnCola);
+            groupBox2.Location = new Point(78, 379);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(871, 206);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Bezalkoholna pića";
+            // 
+            // btnFanta
+            // 
+            btnFanta.Location = new Point(554, 58);
+            btnFanta.Name = "btnFanta";
+            btnFanta.Size = new Size(204, 105);
+            btnFanta.TabIndex = 3;
+            btnFanta.Text = "Fanta";
+            btnFanta.UseVisualStyleBackColor = true;
+            // 
+            // btnSprite
+            // 
+            btnSprite.Location = new Point(304, 58);
+            btnSprite.Name = "btnSprite";
+            btnSprite.Size = new Size(204, 105);
+            btnSprite.TabIndex = 2;
+            btnSprite.Text = "Sprite";
+            btnSprite.UseVisualStyleBackColor = true;
+            // 
+            // btnCola
+            // 
+            btnCola.Location = new Point(52, 58);
+            btnCola.Name = "btnCola";
+            btnCola.Size = new Size(204, 105);
+            btnCola.TabIndex = 1;
+            btnCola.Text = "Coca Cola";
+            btnCola.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(btnSweets);
+            groupBox3.Controls.Add(btnIceCream);
+            groupBox3.Location = new Point(78, 628);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(875, 194);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // btnSweets
+            // 
+            btnSweets.Location = new Point(304, 57);
+            btnSweets.Name = "btnSweets";
+            btnSweets.Size = new Size(204, 105);
+            btnSweets.TabIndex = 2;
+            btnSweets.Text = "Kolač";
+            btnSweets.UseVisualStyleBackColor = true;
+            // 
+            // btnIceCream
+            // 
+            btnIceCream.Location = new Point(52, 57);
+            btnIceCream.Name = "btnIceCream";
+            btnIceCream.Size = new Size(204, 105);
+            btnIceCream.TabIndex = 1;
+            btnIceCream.Text = "Sladoled";
+            btnIceCream.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(btnCupon100);
+            groupBox4.Location = new Point(78, 875);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(875, 203);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Poklon bonovi";
+            // 
+            // btnCupon100
+            // 
+            btnCupon100.Location = new Point(52, 59);
+            btnCupon100.Name = "btnCupon100";
+            btnCupon100.Size = new Size(204, 105);
+            btnCupon100.TabIndex = 1;
+            btnCupon100.Text = "100€";
+            btnCupon100.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(773, 34);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(180, 105);
+            btnClose.TabIndex = 5;
+            btnClose.Text = "Izlaz";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(998, 32);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(802, 790);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // lblSumTxt
+            // 
+            lblSumTxt.AutoSize = true;
+            lblSumTxt.Font = new Font("Segoe UI", 14F);
+            lblSumTxt.Location = new Point(998, 837);
+            lblSumTxt.Name = "lblSumTxt";
+            lblSumTxt.Size = new Size(202, 62);
+            lblSumTxt.TabIndex = 7;
+            lblSumTxt.Text = "Ukupno:";
+            // 
+            // lblSum
+            // 
+            lblSum.AutoSize = true;
+            lblSum.Font = new Font("Segoe UI", 14.1F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSum.Location = new Point(1576, 837);
+            lblSum.Name = "lblSum";
+            lblSum.Size = new Size(224, 62);
+            lblSum.TabIndex = 8;
+            lblSum.Text = "0,00 EUR";
+            lblSum.TextAlign = ContentAlignment.TopRight;
+            // 
+            // btnNew
+            // 
+            btnNew.Location = new Point(998, 973);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(394, 105);
+            btnNew.TabIndex = 9;
+            btnNew.Text = "Novi račun";
+            btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnCheckout
+            // 
+            btnCheckout.Location = new Point(1406, 973);
+            btnCheckout.Name = "btnCheckout";
+            btnCheckout.Size = new Size(394, 105);
+            btnCheckout.TabIndex = 10;
+            btnCheckout.Text = "Naplata";
+            btnCheckout.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1656, 1012);
+            ClientSize = new Size(1837, 1183);
+            Controls.Add(btnCheckout);
+            Controls.Add(btnNew);
+            Controls.Add(lblSum);
+            Controls.Add(lblSumTxt);
+            Controls.Add(richTextBox1);
+            Controls.Add(btnClose);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(lblHeader);
             Name = "Form1";
             Text = "VUB Caffe";
             Paint += Form1_Paint;
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblHeader;
+        private GroupBox groupBox1;
+        private Button btnLatte;
+        private Button btnCappucino;
+        private Button btnEspresso;
+        private GroupBox groupBox2;
+        private Button btnFanta;
+        private Button btnSprite;
+        private Button btnCola;
+        private GroupBox groupBox3;
+        private Button btnSweets;
+        private Button btnIceCream;
+        private GroupBox groupBox4;
+        private Button btnCupon100;
+        private Button btnClose;
+        private RichTextBox richTextBox1;
+        private Label lblSumTxt;
+        private Label lblSum;
+        private Button btnNew;
+        private Button btnCheckout;
     }
 }
--- Zadaca/Form1.cs
+++ Zadaca/Form1.cs
@@ -5,6 +5,14 @@ namespace Zadaca
         public Form1()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.groupBox1.BackColor = Color.Transparent;
+            this.groupBox2.BackColor = Color.Transparent;
+            this.groupBox3.BackColor = Color.Transparent;
+            this.groupBox4.BackColor = Color.Transparent;
+            this.lblSumTxt.BackColor = Color.Transparent;
+            this.lblSum.BackColor = Color.Transparent;
+            this.lblHeader.BackColor = Color.Transparent;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -16,6 +24,21 @@ namespace Zadaca
         System.Drawing.Drawing2D.LinearGradientMode.Horizontal
     );
             e.Graphics.FillRectangle(brush, this.ClientRectangle);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
