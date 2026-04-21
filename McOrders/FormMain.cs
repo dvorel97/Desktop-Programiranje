16f2e3de9c6954314a6e2a6fbceadd5a4fd27cca
David Vorel <dvorel@vub.hr>
2026-04-21 12:02:55 +0200

    LV McD

---
+++ McOrders/FormMain.cs
@@ -0,0 +1,54 @@
+namespace McOrders {
+  public partial class FormMain : Form {
+    Ordering ordering;
+
+    public FormMain() {
+      InitializeComponent();
+      ordering = new Ordering();
+      // TODO: registracija metode koja se pokreæe kada se pokrene narudžba
+      ordering.OrderCompleted += OrderCompleted;
+      ordering.OrderStarted += OrderStarted;
+    }
+
+    private void OrderStarted(object sender, OrderArgs e) {
+      // TODO: Potrebno je pozvati metodu koja ažurira
+      // aktivne narudžbe
+      if(InvokeRequired) {
+        BeginInvoke(UpdateActiveOrders);
+      }
+
+    }
+
+    private void OrderCompleted(object sender, OrderArgs e) {
+      if (InvokeRequired) {
+        BeginInvoke(UpdateCompletedOrders);
+      }
+    }
+
+    private void UpdateActiveOrders() {
+      lbActiveOrders.Items.Clear();
+      lbActiveOrders.Items.AddRange(ordering.ActiveOrders.ToArray());
+    }
+
+    private void UpdateCompletedOrders() {
+      UpdateActiveOrders();
+      lbCompletedOrders.Items.Clear();
+      lbCompletedOrders.Items.AddRange(ordering.CompletedOrders.ToArray());
+    }
+
+    private void btnPommes_Click(object sender, EventArgs e) {
+        ordering.AddOrder(new FoodPommes());
+
+            // TODO: narudžba pommesa
+        }
+
+    private void btnBigMac_Click(object sender, EventArgs e) {
+            ordering.AddOrder(new FoodBigMac());
+            // TODO: narudžba bigmaca
+        }
+
+    private void btnDrink_Click(object sender, EventArgs e) {
+      ordering.AddOrder(new FoodDrink());
+    }
+  }
+}
---
+++ McOrders/Ordering.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Design;
+
+namespace McOrders {
+  public class Ordering {
+    public Ordering() {
+        nextOrderId = 1;
+        CompletedOrders = new List<Order>();
+        ActiveOrders = new List<Order>();
+    }
+
+    public void AddOrder(Food food) {
+      var order = new Order(nextOrderId++, food);
+      Task task = Task.Run(() => {
+        Thread.Sleep(2000);
+        StartOrder(order);
+        Thread.Sleep(order.Food.PreparingTime * 1000);
+        CompleteOrder(order);
+      });
+    }
+
+    public void StartOrder(Order order) {
+      ActiveOrders.Add(order);
+      OrderStarted?.Invoke(this, new OrderArgs(order));
+    }
+
+    // TODO: Metoda koja prebacuje narudžbu iz aktivnih u završene
+    // TODO: Mora pokrenuti događaj koji signalizira završetak
+    // narudžbe
+    public void CompleteOrder(Order order) {
+        if (ActiveOrders.Contains(order))
+        {
+            ActiveOrders.Remove(order);
+        }
+        CompletedOrders.Add(order);
+        OrderCompleted?.Invoke(this, new OrderArgs(order));
+        }
+
+    public delegate void OrderingDelegate(object sender, OrderArgs e);
+    public event OrderingDelegate OrderStarted;
+    public event OrderingDelegate OrderCompleted;
+    // TODO: Događaj za završene narudžbe
+
+    public List<Order> ActiveOrders { get; }
+    public List<Order> CompletedOrders { get; }
+        // TODO: List završenih narudžbi
+
+        private int nextOrderId;
+  }
+}
